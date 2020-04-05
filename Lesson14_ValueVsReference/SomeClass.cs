17a5e31044cba6f4d38b5232f7bebfec18f85bc2
PeterShipp <pshipp81@aol.com>
2020-04-05 10:23:01 -0700

    Add project files.

---
+++ Lesson14_ValueVsReference/SomeClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson14_ValueVsReference
+{
+    class SomeClass
+    {
+        public int myNumber = 10;
+    }
+}
